65821a3662820b5221fdb41970dff3731ed6e62a
OuroPhantom <148102564+OuroPhantom@users.noreply.github.com>
2024-12-01 13:12:32 -0600

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: 'http://localhost:4000/api',
-    baseURL: 'https://apiproductos-dovt.onrender.com/api',
+    baseURL: 'https://msc-backend-1.onrender.com',
     withCredentials: true,
     headers:{
         Accept: 'application/json'
